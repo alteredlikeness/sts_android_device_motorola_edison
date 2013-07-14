e503edc5f1c4a71ec36f10c2e865225071816d64
alteredlikeness <alteredlikeness@gmail.com>
2013-07-14 10:17:39 -0500

    device updates (inits, cmdline, etc)

--- device.mk
+++ device.mk
@@ -25,21 +25,24 @@ PRODUCT_PACKAGES += \
 
 # Kexec rootfs files
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/init.mapphone.rc:root/init.mapphone_cdma.rc \
-    $(DEVICE_FOLDER)/init.mapphone.rc:root/init.mapphone_umts.rc \
-    $(DEVICE_FOLDER)/init.target.rc:root/init.target.rc \
+    $(DEVICE_FOLDER)/root/init.mapphone.rc:root/init.mapphone_umts.rc \
+    $(DEVICE_FOLDER)/root/init.mapphone.rc:root/init.mapphone_cdma.rc \
+    $(DEVICE_FOLDER)/root/init.target.rc:root/init.target.rc \
+    $(DEVICE_FOLDER)/root/init.rc:root/init.rc \
+    $(DEVICE_FOLDER)/root/init:root/init \
+    $(DEVICE_FOLDER)/root/ueventd.rc:root/ueventd.rc \
     $(DEVICE_FOLDER)/kexec/devtree:system/etc/kexec/devtree \
     $(OUT)/ramdisk.img:system/etc/kexec/ramdisk.img \
     $(OUT)/kernel:system/etc/kexec/kernel
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/apns-conf.xml:system/etc/apns-conf.xml \
     $(DEVICE_FOLDER)/media_profiles.xml:system/etc/media_profiles.xml \
     $(DEVICE_FOLDER)/vold.fstab:system/etc/vold.fstab
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/edison/edison-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/edison/edison-vendor-stock-camera.mk)
 $(call inherit-product-if-exists, vendor/motorola/edison/edison-vendor-stock-ducati.mk)
 $(call inherit-product-if-exists, vendor/motorola/omap4-common/common-vendor-pvr.mk)
 
--- full_edison.mk
+++ full_edison.mk
@@ -25,13 +25,7 @@
 PRODUCT_PACKAGES := \
     Gallery
 
-#if we do this after the full_base_telephony is included some of these don't get picked up..
-ifeq ($(TARGET_PRODUCT),full_edison)
-PRODUCT_COPY_FILES += \
-    device/motorola/edison/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
-endif
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=edison
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=umts_edison
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
